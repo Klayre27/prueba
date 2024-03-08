874ebad35a15c1f0f6afac8c96a93775638bfca1
Klayre27 <159667610+Klayre27@users.noreply.github.com>
2024-03-08 14:40:13 -0500

    Add files via upload

---
+++ programacion2.py
@@ -0,0 +1,26 @@
+# Definir la matriz bidimensional
+matriz = [
+    [1, 3, 5],
+    [7, 9, 11],
+    [13, 15, 17]
+]
+
+# Función para buscar un valor específico en la matriz
+def buscar_valor(matriz, valor):
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            if matriz[i][j] == valor:
+                return True, i, j
+    return False, None, None
+
+# Valor a buscar en la matriz
+valor_a_buscar = 5
+
+# Realizar la búsqueda en la matriz
+encontrado, fila, columna = buscar_valor(matriz, valor_a_buscar)
+
+# Imprimir el resultado de la búsqueda
+if encontrado:
+    print(f"El valor {valor_a_buscar} fue encontrado en la fila {fila+1} y columna {columna+1}.")
+else:
+    print(f"El valor {valor_a_buscar} no fue encontrado en la matriz.")
